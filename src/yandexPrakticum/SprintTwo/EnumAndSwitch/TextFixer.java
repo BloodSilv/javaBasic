0ddfc37ce8dbe3080b43bc06fce912228140919f
BloodSilv <chvn@live.ru>
2022-03-16 17:58:11 +0500

    1

---
+++ src/yandexPrakticum/SprintTwo/EnumAndSwitch/TextFixer.java
@@ -0,0 +1,21 @@
+package yandexPrakticum.SprintTwo.EnumAndSwitch;
+
+class TextFixer {
+
+    public String fixText(String text) {
+        // Реализуй метод
+
+        text= text.replace("полю", "лесу").replace("разозлился",
+                "рассердился").replace("хлоп", "топ");
+        return text;
+    }
+
+    public static void main(String[] args) {
+
+        String text = "Мишка косолапый по полю идёт, шишки собирает, песенки поёт. Шишка отскочила прямо мишке в лоб. Мишка разозлился и ногою - хлоп!";
+
+        var textFixer = new TextFixer();
+        var fixedText = textFixer.fixText(text);
+        System.out.println(fixedText);
+    }
+}
